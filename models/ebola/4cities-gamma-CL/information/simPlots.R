2211988cc8bf8cc911d13b44a898696aba0ba66d
Alejandro Bernardin <bernardin.alejandro@gmail.com>
2016-03-15 14:14:07 -0400

    - Added more infection/information options - Fix some code

--- models/ebola/4cities-gamma-CL/information/simPlots.R
+++ models/ebola/4cities-gamma-CL/information/simPlots.R
@@ -1,26 +1,17 @@
-#city<-read.table("base.simdir/Santiago.out", col.names=c("time", "cases", "susceptible", "exposed", "infected", "removed", "dead"))
 Santiago<-read.table("base.simdir/Santiago.out", col.names=c("time", "cases", "susceptible", "exposed", "infected", "removed", "dead"))
 Valparaiso <-read.table("base.simdir/Valparaiso.out", col.names=c("time", "cases", "susceptible", "exposed", "infected", "removed", "dead"))
 Rancagua <-read.table("base.simdir/Rancagua.out", col.names=c("time", "cases", "susceptible", "exposed", "infected", "removed", "dead"))
 La_Serena <-read.table("base.simdir/La_Serena.out", col.names=c("time", "cases", "susceptible", "exposed", "infected", "removed", "dead"))
 
 cities <- Santiago + Valparaiso + Rancagua + La_Serena
-write.table(cities,"totalCities.out")
+write.table(cities,"base.simdir/totalCities.out")
 
 plot(cities $time, cities $cases, ann=F,col="red")
 points(cities $time, cities $susceptible, ann=F, col="green")
 points(cities $time, cities $exposed, ann=F,col="blue")
-#plot(cities $time, cities $exposed, ann=F,col="blue")
 points(cities $time, cities $infected, ann=F, col="black")
 points(cities $time, cities $removed, ann=F, col="yellow")
 points(cities $time, cities $dead, ann=F, col="orange")
-
-#library(data.table)
-#CIT <- data.table(cities)
-#toPlot<-CIT[, list(time, deadDiff=diff(dead),removeDiff=diff(removed))]
-#points(toPlot $time, toPlot $removeDiff, col="yellow")
-#points(toPlot $time, toPlot $deadDiff, col="orange")
-
 
 legend("topleft", inset=.05, title="State",c("E+I+R+D","susceptible","exposed","infected","removed","dead"), fill=c("red","green","blue","black","yellow","orange"), horiz=FALSE)
 
