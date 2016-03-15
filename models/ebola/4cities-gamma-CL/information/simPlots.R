e7a5d41d9b14cc22f3859fec3891ba8b2cf3f09c
Alejandro Bernardin <bernardin.alejandro@gmail.com>
2016-03-15 14:22:44 -0400

    - Replaced some numbers by constant - Added graph

--- models/ebola/4cities-gamma-CL/information/simPlots.R
+++ models/ebola/4cities-gamma-CL/information/simPlots.R
@@ -10,10 +10,10 @@ plot(cities $time, cities $cases, ann=F,col="red")
 points(cities $time, cities $susceptible, ann=F, col="green")
 points(cities $time, cities $exposed, ann=F,col="blue")
 points(cities $time, cities $infected, ann=F, col="black")
-points(cities $time, cities $removed, ann=F, col="yellow")
+points(cities $time, cities $removed, ann=F, col="purple")
 points(cities $time, cities $dead, ann=F, col="orange")
 
-legend("topleft", inset=.05, title="State",c("E+I+R+D","susceptible","exposed","infected","removed","dead"), fill=c("red","green","blue","black","yellow","orange"), horiz=FALSE)
+legend("topleft", inset=.05, title="State",c("E+I+R+D","susceptible","exposed","infected","removed","dead"), fill=c("red","green","blue","black","purple","orange"), horiz=FALSE)
 
 title(main="States", sub="time", ylab="cases")
 
